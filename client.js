4d2ce38abb78b61ce267c32b1420400c39a3d667
Joelle Kado <jkado@my.hpu.edu>
2020-11-09 16:59:31 -0600

    got people and pictures on DOM

--- client.js
+++ client.js
@@ -1,1 +1,29 @@
 console.log('Here are all the available people:', people);
+
+console.log('JS is CONNECTED');
+
+$(document).ready(function() {
+    console.log('JQ is READY');
+
+    for(person of people) {
+        $('#namePlace').append(`<div>${person.name} <img src="https://github.com/${person.githubUsername}.png?size=250"></div> 
+        `)
+    }
+    // $('#generateButton').on('click', appendDivFunction);
+    // /*append a `<div>` element when you click the button. 
+    // It should append a new div each time you click the button.*/
+    // /*Clicking a "Yellow" `<button>` should change its parent `background-color` 
+    // from red to yellow.*/
+    // $('#generateButton').on('click', '.yellowButton', yellowFunction);
+
+    console.log(randomNumber(0, 6));
+});
+
+
+
+function randomNumber(min, max){//this is the index
+
+
+    return Math.floor(Math.random() * (1 + max - min) + min);
+    
+}
