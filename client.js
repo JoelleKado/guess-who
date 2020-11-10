178c4f0fbc6e96d2ef3c693556b2c07c89d94d70
Joelle Kado <jkado@my.hpu.edu>
2020-11-09 21:19:49 -0600

    Assignment Submission

--- client.js
+++ client.js
@@ -2,30 +2,54 @@ console.log('Here are all the available people:', people);
 
 console.log('JS is CONNECTED');
 
-$(document).ready(function() {
+$(document).ready(function () {
     console.log('JQ is READY');
 
-    for(person of people) {
+     for (person of people) {//Get everyone on the DOM
         $('#namePlace').append(`<div>${person.name} <img src="https://github.com/${person.githubUsername}.png?size=250"></div> 
-        `)
-    }
+        `);
+    };
     // $('#generateButton').on('click', appendDivFunction);
     // /*append a `<div>` element when you click the button. 
     // It should append a new div each time you click the button.*/
     // /*Clicking a "Yellow" `<button>` should change its parent `background-color` 
     // from red to yellow.*/
     // $('#generateButton').on('click', '.yellowButton', yellowFunction);
+    nameGenerator(0, 2);
+    console.log('The answer is:', answer);
+    phaseTwo();
 
-    console.log(randomNumber(0, 6));
-});
+});//EXIT readyNow
 
+let answer;
 
-
-
-function randomNumber(min, max){//this is the index
+function nameGenerator(min, max) {//ENTER randomNumberGenerator
     let generatedNumber = Math.floor(Math.random() * (1 + max - min) + min);
-    if(generatedNumber ===3){console.log('the number is 3');
+    // if(generatedNumber ===3){
+    //   console.log('the number is 3');
+    //console.log(generatedNumber);
+    //console.log(generatedNumber);
+    if (generatedNumber === 0) {
+        answer = 'Dev';
+        console.log(answer);
+    }
+    if (generatedNumber === 1) {
+        answer = 'Dane';
+        console.log(answer);
+
+    }
+    if (generatedNumber === 2) {
+        answer = 'Mary';
+        console.log(answer);
     }
     return generatedNumber;
-    
-}
+};//EXIT randomNumberGenerator
+
+function phaseTwo() {//ENTER phaseTwo
+    console.log('Round One!');
+    // if (answer = 'Dev') {
+    //append name to instructions place
+    $('#instructionsPlace').append(`<h1>Click on: ${answer}</h1>`);
+    // }
+
+};//EXIT phaseTwo
